51698894f220376736c89177b5d709f4e175fc48
xc <xc_we@qq.com>
2024-05-02 14:21:28 +0800

    bugfix - passwd 逻辑判断错误

--- config/sk.py
+++ config/sk.py
@@ -7,16 +7,17 @@ __all__ = [
 ]
 
 def create_pk(passwd=None):
-    if passwd:
+    if not passwd:
         sk = Fernet.generate_key()
-        print(f'Keep the password {passwd} or/and {sk_file} some place safe! \n'
+        print(f'Keep the {sk_file} some place safe! \n'
               'If you lose it you’ll no longer be able to decrypt messages.\n')
     else:
         import hashlib
         import base64
         sk = base64.urlsafe_b64encode(hashlib.sha256(passwd.encode()).digest())
-        print(f'Keep the {sk_file} some place safe! \n'
+        print(f'Keep the password {passwd} or/and {sk_file} some place safe! \n'
               'If you lose it you’ll no longer be able to decrypt messages.\n')
+
     with open(sk_file, 'wb') as f:
         f.write(sk)
 
